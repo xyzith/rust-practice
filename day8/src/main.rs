740bb589b87b64ac9b7820681181310fda101dfd
Taylor Tang <taylor.t.tcg@gmail.com>
2026-03-06 12:23:10 +0800

    day8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,13 @@
+// Array 版本：固定大小，回傳 Vec
+fn filter_even_array(arr: [i32; 5]) -> Vec<i32> {
+    arr.iter().copied().filter(|&x| x % 2 == 0).collect()
+}
+
+// Vec 版本：動態大小，修改原地或回傳新的
+fn filter_even_vec(vec: &Vec<i32>) -> Vec<i32> {
+    return vec.iter().filter(|&&x| x % 2 == 0).copied().collect();
+}
+
 fn sum_array(arr: [i32; 5]) -> i32 {
     return arr.iter().sum();
     // let mut sum = 0;
@@ -22,4 +32,10 @@ fn main() {
 
     let vec = vec![10, 20, 30];
     println!("Vec sum: {}", sum_vec(vec));
+
+    let arr = [1, 2, 3, 4, 5];
+    println!("Array evens: {:?} orig: {:?}", filter_even_array(arr), arr);
+
+    let vec = vec![10, 15, 20, 25, 30];
+    println!("Vec evens: {:?} orig: {:?}", filter_even_vec(&vec), vec);
 }
